3eb367772632474444cc0d8837aef8048ab25b63
Vadim <vadim.prog.2000@gmail.com>
2020-10-18 18:08:32 +0300

    add interact

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,16 +1,37 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-function Article(props) {
-	const {article} = props
-	const body = <section>{article.text}</section>
-	
-	return  (
-		<div className="hello" syle={{color: 'red'}}>
-			<h2>{article.title}</h2>
-			{body}
-			<h3>creation date: {(new Date(article.date)).toDateString()}</h3>
-		</div>
-	)
+class Article extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			isOpen: true
+		}
+	}
+
+	render() {
+		const {article} = this.props
+		const body = this.state.isOpen && <section>{article.text}</section>
+
+		return  (
+			<div>
+				<h2>
+					{article.title}
+					<button onClick={this.handleClick}>
+						{this.state.isOpen ? "Close" : "Open" }
+					</button>
+				</h2>
+				{body}
+				<h3>creation date: {(new Date(article.date)).toDateString()}</h3>
+			</div>
+		)
+	}
+
+	handleClick = () => {
+		this.setState({
+			isOpen: !this.state.isOpen
+		})
+	}
 }
 
 export default Article
